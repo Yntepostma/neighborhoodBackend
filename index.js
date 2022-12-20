b4bc5d2a96af9dacfe45ba7305842314fcb4c3fb
Yntepostma <ynte.postma@gmail.com>
2022-12-20 19:29:30 +0100

    socket-io setup done

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require("express");
-const { Server } = require("socket.io");
 const corsMiddleWare = require("cors");
 const PORT = 4000;
+const app = express();
+
+
+const http = require('http').Server(app)
+
+const socketIO = require('socket.io')(http, {
+  cors: {
+      origin: "http://localhost:3000"
+  }
+});
+
+socketIO.on('connection', (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  socket.on('disconnect', () => {
+    console.log('🔥: A user disconnected');
+  });
+  socket.on('message', (data) => {
+    console.log(data);
+  });
+  socket.on('disconnect', () => {
+    console.log('🔥: A user disconnected');
+  });
+});
 
 const Event = require("./models").event;
 const Category = require("./models").category;
@@ -9,7 +31,7 @@ const neighborhoodsRouter = require("./Routers/neighborhoods");
 const authRouter = require("./Routers/auth");
 const eventRouter = require("./Routers/events");
 const marketplaceRouter = require("./Routers/marketplace");
-const app = express();
+
 
 app.use(corsMiddleWare());
 app.use(express.json());
@@ -17,22 +39,6 @@ app.use("/neighborhoods", neighborhoodsRouter);
 app.use("/auth", authRouter);
 app.use("/events", eventRouter);
 app.use("/marketplace", marketplaceRouter);
-
-const http = require("http");
-const server = http.createServer(app);
-const io = new Server(server);
-let messages = [];
-
-io.on("connection", (socket) => {
-  socket.on("getPreviousMessages", () => {
-    socket.emit("receivePreviousMessages", messages);
-  });
-
-  socket.on("sendMessage", (name, message) => {
-    messages.push({ name, message });
-    io.emit("receiveMessage", name, message);
-  });
-});
 
 app.get("/", async (req, res) => {
   const eventCategories = await Event.findAll({
@@ -42,7 +48,7 @@ app.get("/", async (req, res) => {
   console.log(eventCategories);
 });
 
-server.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
 
